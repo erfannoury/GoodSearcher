0271538b0d71aafb142d152941f7f9ad07ce3a35
Erfan Noury <erfannoury@users.noreply.github.com>
2015-01-25 23:25:25 +0330

    Added pyLucene searching to the searcher module :tada:

--- searcher.py
+++ searcher.py
@@ -24,7 +24,7 @@ class index:
             page = int(data_input["page"])
         render = web.template.render('templates/')
         anses = []
-
+        num_pages = 0
         if use_elasticsearch:
             # importing libraries for Elasticsearch
             from elasticsearch import Elasticsearch
@@ -48,7 +48,47 @@ class index:
                 anses.append({'title':res.title, 'description': res.description.encode('utf-8'), 'url': res.url, 'cover':res.cover, 'authors':authors})
         else:
             # importing libraries for Lucene
-            pass
+            import lucene
+            from java.io import File
+            from org.apache.lucene.index import DirectoryReader, Term
+            from org.apache.lucene.queryparser.classic import QueryParser
+            from org.apache.lucene.store import SimpleFSDirectory
+            from org.apache.lucene.search import IndexSearcher, BooleanClause, BooleanQuery, TermQuery
+            from org.apache.lucene.util import Version
+            from org.apache.lucene.analysis.standard import StandardAnalyzer
+            import os
+
+            # fields
+            title_field = 'title'
+            description_field = 'description'
+            cover_field = 'cover'
+            authors_name_field = 'authors_name'
+            authors_url_field = 'authors_url'
+            url_field = 'url'
+
+            index_folder = '.'
+            index_name = 'lucene.index'
+            index_path = os.path.join(index_folder, index_name)
+
+            lucene.initVM()
+            version = Version.LUCENE_CURRENT
+            directory = SimpleFSDirectory(File(index_path))
+            searcher = IndexSearcher(DirectoryReader.open(directory))
+            analyzer = StandardAnalyzer(version)
+
+            title_tq = TermQuery(Term(title_field, query))
+            desc_tq = TermQuery(Term(description_field, query))
+            query = BooleanQuery()
+            query.add(BooleanClause(title_tq, BooleanClause.Occur.SHOULD))
+            query.add(BooleanClause(desc_tq, BooleanClause.Occur.SHOULD))
+            scoreDocs = searcher.search(query, 1000).scoreDocs
+            num_pages = (len(scoreDocs) / 10) + 1
+
+            for scoreDoc in scoreDocs[page*10 : page * 10 + 10]:
+                doc = searcher.doc(scoreDoc.doc)
+                authors = zip([doc.get(authors_name_field)], [doc.get(authors_url_field)])
+                anses.append({'title':doc.get(title_field), 'description': doc.get(description_field).encode('utf-8'), 'url': doc.get(url_field), 'cover':doc.get(cover_field), 'authors':authors})
+
         return render.index(anses, query, num_pages)
 
 if __name__ == "__main__":
